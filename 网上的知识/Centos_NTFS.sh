a7ad8ab1e3fb56b80a34a72e5e6c5417e32b8812
youjiahe <youjiahe@163.com>
2018-10-23 18:54:01 +0800

    2018-10-23_18:54

--- 网上的知识/Centos_NTFS.sh
+++ 网上的知识/Centos_NTFS.sh
@@ -1,4 +1,5 @@
 1.切换到系统yum目录并下载阿里的
 [root@localhost ~]# cd /etc/yum.repos.d/
 [root@localhost yum.repos.d]# wget http://mirrors.aliyun.com/repo/epel-7.repo
-
+2.
+[root@localhost yum.repos.d]# yum -y install ntfs-3g
