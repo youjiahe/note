5aac4c4b0c083e0272fb9b80ef0ea5212de7f27c
youjiahe <youjiahe@163.com>
2018-11-03 20:30:01 +0800

    2018-11-03_20:30

--- 14.Review/Re3.sh
+++ 14.Review/Re3.sh
@@ -34,9 +34,44 @@ baseurl=ftp://192.168.1.242/centos7
 enable=1
 gpgcheck=0
 
+##################################################################################
+管理用户和组
+脚本：创建用户，用户名为user1 密码123456 首次登陆强之修改密码；执行脚本默认创建5个用户，也可以通过位置参数指定创建多少个用户
 
+nn=0
 
+if  [ -z $1 ]; then
+    num=5
+elif [[ ! $1 =~ ^[0-9]+$ ]]; then                
+#正则匹配
+    echo "Input invaild,must be number" >&2
+    exit 2
+fi
 
+num=$1
+
+while [ $c -lt $num ] &>/dev/null 
+  do
+        id user$n &>/dev/null
+        if [ $? -ne 0 ]; then
+            nn=$n
+            useradd user$nn  &>/dev/null
+            echo 123456 | passwd --stdin user$nn &>/dev/null
+            chage -d 0 user$nn
+            let c++
+            echo -en "create user${nn}\t"
+            echo -e "\e[32m[done]\e[0m"
+        else
+            let n++
+        fi
+  done
+##################################################################################
+权限
+chmod 755 1.txt
+
+SUID:以所有者身份执行命令
+SGID
+Sticky bit
 
 
 
