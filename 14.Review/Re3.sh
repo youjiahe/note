8ee6797c2cca17a2db71481382fcae8e0d07e056
youjiahe <youjiahe@163.com>
2018-11-03 19:10:58 +0800

    最新笔记

--- 14.Review/Re3.sh
+++ 14.Review/Re3.sh
@@ -1,2 +1,64 @@
+第一阶段复习
 ##################################################################################
-●
+管道
+●tr
+[root@youjiahe ~]# cat a.txt | tr 'a-z' 'A-Z'
+ABCDEFG
+HIJKLM
+OOO
+[root@youjiahe ~]# cat a.txt | tr 'A-Z' 'a-z'
+abcdefg
+hijklm
+ooo
+[root@youjiahe ~]# cat a.txt 
+abcDefg
+hijklm
+ooo
+
+[root@youjiahe ~]# echo 123awt125 | tr -cd [0-9]
+123125
+
+●重定向
+[root@youjiahe ~]# cat <<EOF > /opt/1.txt
+[development]
+name=centos
+baseurl=ftp://192.168.1.242/centos7
+enable=1
+gpgcheck=0
+EOF
+
+[root@youjiahe ~]# cat /opt/1.txt 
+[development]
+name=centos
+baseurl=ftp://192.168.1.242/centos7
+enable=1
+gpgcheck=0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
