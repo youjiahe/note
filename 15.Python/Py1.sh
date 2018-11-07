941ec60c7f15f5d2dd0f45cc52703aff5420f660
youjiahe <youjiahe@163.com>
2018-11-07 18:00:01 +0800

    2018-11-07_18:00

--- 15.Python/Py1.sh
+++ 15.Python/Py1.sh
@@ -9,8 +9,9 @@ Python自动化运维 day01
 4.python简介
    4.1 环境
    4.2 pycharm
-5. git
-
+5.git
+6.python运行方式
+7.Python语法结构
 ##############################################################################
 开班
 ●教学支持
@@ -70,14 +71,49 @@ Guido van Rossum 作者  1991年
 3.  安装python3,使其支持Tab键补全
 4.  配置pycharm,使其符合自己的习惯
 ##############################################################################
-git
+git #老师用这个来共享代码
 ● 案例2:配置git
 1.  安装git版本控制软件
 2.  设置用户信息,如用户名、email等
 3.  设置默认编辑器为vim
 4.  查看用户配置
-
-● 老师的git
+##############################################################################
+git 客户端使用
+● 命令汇总
+git config --global user.name "youjiahe"
+git config --global user.email "youjiahe@163.com"
+git config --global core.editor vim
+git config --list
+git init
+git add <files> /git add . /git add --all
+git status .
+git commit -m "first_commit"
+git commit -am "first_commit"   #先add，再commit
+git ls-files
+git rm <files>  #删除版本库文件
+git remote rename origin old-origin
+git remote remove origin
+git remote add origin git@192.168.4.1:nsd1806/core_py.git
+git push -u origin --all
+git clone git@192.168.4.1:nsd1806/core_py.git
+git pull
+##############################################################################
+git 客户端使用
+● git pull 与 git clone 的区别
+//clone 是本地没有 repository 时，将远程 repository 整个下载过来。
+//pull 是本地有 repository 时，将远程 repository 里新的 commit 数据(如有的话)下载过来。
+##############################################################################
+git 客户端使用
+● git常见问题
+1.权限问题
+  chmod 600 id_rsa
+2.host解析问题
+   git remote add origin git@192.168.4.1:/nsd1806/core_py.git
+3.将密钥交给代理
+   ssh-agent   #下面有命令解析
+   ssh-add  
+##############################################################################
+● git config -global
 [root@room9pc01 ~]# git config --global user.name "youjiahe"
 [root@room9pc01 ~]# git config --global user.email "youjiahe@163.com"
 [root@room9pc01 ~]# git config --global core.editor vim
@@ -100,18 +136,8 @@ git
     暂存区有时也叫作索引(index)
 •  版本库:工作区有一个隐藏目录.git,这个不算工作
    区,而是Git的版本库
+
 ##############################################################################
-git 客户端使用
-● 命令
-git init
-git add <files> /git add . /git add --all
-git status .
-git commit -m "first_commit"
-git commit -am "first_commit"   #先add，再commit
-git ls-files
-git rm <files>  #删除版本库文件
-
-
 ● git使用
 & 创建自己的版本库
 [root@room9pc01 python]# mkdir youjiahe
@@ -157,21 +183,11 @@ welcome.py
 hello.py
 
 ##############################################################################
-搭建git 服务端
+搭建gitlab 服务端
 环境准备
 ● 准备一台虚拟机 4G内存 40G硬盘
 & 装包
 [root@python_git docker_pkgs]# ls  #老师提供
-container-selinux-2.42-1.gitad8f0f7.el7.noarch.rpm
-container-storage-setup-0.8.0-3.git1d27ecf.el7.noarch.rpm
-docker-1.13.1-53.git774336d.el7.centos.x86_64.rpm
-docker-client-1.13.1-53.git774336d.el7.centos.x86_64.rpm
-docker-common-1.13.1-53.git774336d.el7.centos.x86_64.rpm
-oci-register-machine-0-6.git2b44233.el7.x86_64.rpm
-oci-systemd-hook-0.1.15-2.gitc04483d.el7.x86_64.rpm
-oci-umount-2.3.3-3.gite3c9055.el7.x86_64.rpm
-skopeo-containers-0.1.28-1.git0270e56.el7.x86_64.rpm
-
 [root@python_git docker_pkgs]# yum -y install *.rpm
 
 & 导入镜像
@@ -183,7 +199,7 @@ skopeo-containers-0.1.28-1.git0270e56.el7.x86_64.rpm
 [root@python_git ~]# vim /etc/ssh/sshd_config #修改22为2222
 [root@python_git ~]# systemctl restart sshd
 ##############################################################################
-Gitlab使用
+Gitlab启动
 ●将docker主机ssh端口改为2222后,起动容器
 [root@python_git ~]# docker run -d -h gitlab \
 --name gitlab \
@@ -201,9 +217,11 @@ firefox 192.168.4.1  #会看到"GitLab 中文社区版"
 
 ●密码大于8位，用root登陆、创建群组、创建项目、创建用户、添加用户到组
 ##############################################################################
+git
+推送文件到远程  #有密码
 ●创建远程仓库
 [root@room9pc01 youjiahe]# git remote rename origin old-origin 
-error: 不能重命名配置小节 'remote.origin' 到 'remote.old-origin'
+error: 不能重命名配置小节 'remote.origin' 到 'remote.old-origin'  #不用管
 [root@room9pc01 youjiahe]# git remote add origin http://192.168.4.1/nsd1806/core_py.git
  
 ●往刚刚创建的项目中推送文件
@@ -213,7 +231,9 @@ Password for 'http://root@192.168.4.1':                          do
 
 ●回到浏览器项目页面查看内容
  可以看到仓库文件
-
+##############################################################################
+git
+推送文件到远程准备  #无密码
 ●ssh免密钥登陆
  粘贴密钥到设置----ssh密钥
 
@@ -221,14 +241,71 @@ Password for 'http://root@192.168.4.1':                          do
 [root@room9pc01 youjiahe]# ssh-agent   #启动ssh代理程序
 [root@room9pc01 youjiahe]# ssh-add     #添加私钥，让代理程序托管
 
-ssh-agent 用于管理 ssh private keys，目的是对解密的私钥进行高速缓存。 
-ssh-add 提示并将用户使用的私钥添加到 ssh-agent 维护列表中，此后当公钥连接到远程 SSH 或 SCP 主机时，不再提示信息。
-ssh-add 命令是把专用密钥添加到ssh-agent的高速缓存中。该命令位置在/usr/bin/ssh-add。
+//ssh-agent 用于管理 ssh private keys，目的是对解密的私钥进行高速缓存。 
+//ssh-add 提示并将用户使用的私钥添加到 ssh-agent 维护列表中，此后当公钥连接到远程 SSH 或 SCP 主机时，不再提示信息。
+//ssh-add 命令是把专用密钥添加到ssh-agent的高速缓存中。该命令位置在/usr/bin/ssh-add。
 
 ●管理区域(小扳手)----project-----选一个项目
    可以看到多了 ssh一行
+
+●无密码方式推送文件
+[root@room9pc01 youjiahe]# git remote remove origin #
+[root@room9pc01 youjiahe]# git remote add origin git@192.168.4.1:nsd1806/core_py.git
+[root@room9pc01 youjiahe]# cp /etc/hosts .
+[root@room9pc01 youjiahe]# git add .
+[root@room9pc01 youjiahe]# git status
+[root@room9pc01 youjiahe]# git commit -m "add files"
+[root@room9pc01 youjiahe]# git push origin master
 ##############################################################################
-●
+git
+●从远程仓库下载本机
+[root@room9pc01 tedu]# rm -rf youjiahe/
+[root@room9pc01 tedu]# git clone git@192.168.4.1:nsd1806/core_py.git
+##############################################################################
+Python运行方式
+● 解释器
+  & python   #python2
+  & python3  #python3
+● 运行方式  
+  & 进入解释器
+    [root@room9pc01 python]# python3  # exit() 、ctrl+d都可以推出
+    Python 3.6.1 (default, Mar 20 2018, 00:12:35)
+    >>> print('hello world')  
+  & 命令行运行  
+   [root@room9pc01 core_py]# python3 print.py 
+    hello world  
+  & 文件指定解释器运行
+    [root@room9pc01 core_py]# cat ./print.py
+     #!/usr/bin/env python3
+     print('hello world')
+    [root@room9pc01 core_py]# chmod +x print.py   #给
+    [root@room9pc01 core_py]# ./print.py        
+    hello world    
+############################################################################## 
+Python语法结构
+1.语法块缩进
+  –  4个空格:非常流行, 范·罗萨姆支持的风格  #可读性
+2.注释及续行
+  #   #“#”为注释
+  \   #续行符，解释时视为同一行
+3.同行多个语句
+  ；  #用“；”分隔
+############################################################################## 
+● print语句
+print(values,...,sep=' ',end='\n')
+//sep是两个值间的值
+//end是最后一个值后添加的字符串
+
+● input语句
+//作用：
+
+
+
+
+
+
+
+
 
 
 
