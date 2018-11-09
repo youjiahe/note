5c38cda8ffdfdda4ca452c3f0962b73d4c4a7773
youjiahe <youjiahe@163.com>
2018-11-09 18:00:22 +0800

    update_2018-11-09_18:00

--- 15.Python/Py1.sh
+++ 15.Python/Py1.sh
@@ -52,56 +52,56 @@ Guido van Rossum 作者  1991年
 设置pycharm  #教学环境默认有
 ● Pycharm是由JetBrains打造的一款Python IDE
 ● 支持的功能有:
-–  调试、语法高亮
-–  Project管理、代码跳转
-–  智能提示、自动完成
-–  单元测试、版本控制
+	–  调试、语法高亮
+	–  Project管理、代码跳转
+	–  智能提示、自动完成
+	–  单元测试、版本控制
 ● 下载地址:https://www.jetbrains.com/pycharm/download
 
 ● 使用pycharm
-打开pycharm
-选择create new project
-在左上角找到file-----setting----editor
-找到editor——font——size
-解释器editor—project——project interpreter
-左侧小齿轮Add local
-选择虚拟环境 /usr/local/bin/python3.6  添加3.6
+	打开pycharm
+	选择create new project
+	在左上角找到file-----setting----editor
+	找到editor——font——size
+	解释器editor—project——project interpreter
+	左侧小齿轮Add local
+	选择虚拟环境 /usr/local/bin/python3.6  添加3.6
 ##############################################################################
 环境准备
 案例1:准备python开发环境
-1.  下载最新版本的python3
-2.  下载pycharm社区版
-3.  安装python3,使其支持Tab键补全
-4.  配置pycharm,使其符合自己的习惯
+	1.  下载最新版本的python3
+	2.  下载pycharm社区版
+	3.  安装python3,使其支持Tab键补全
+	4.  配置pycharm,使其符合自己的习惯
 ##############################################################################
 git #老师用这个来共享代码
 ● 案例2:配置git
-1.  安装git版本控制软件
-2.  设置用户信息,如用户名、email等
-3.  设置默认编辑器为vim
-4.  查看用户配置
+	1.  安装git版本控制软件
+	2.  设置用户信息,如用户名、email等
+	3.  设置默认编辑器为vim
+	4.  查看用户配置
 ##############################################################################
 git 客户端使用
 ● 命令汇总
-git config --global user.name "youjiahe"
-git config --global user.email "youjiahe@163.com"
-git config --global core.editor vim
-git config --list
-git init
-git add <files> /git add . /git add --all
-git status .
-git commit -m "first_commit"
-git commit -am "first_commit"   #先add，再commit
-git ls-files
-git rm <files>  #删除版本库文件
-git remote rename origin old-origin
-git remote remove origin
-git remote add origin git@192.168.4.1:nsd1806/core_py.git
-git push -u origin --all
-git clone git@192.168.4.1:nsd1806/core_py.git
-git pull
-git reset HEAD hello.py    #撤出暂存区 
-git checkout -- hello.py   #撤出后恢复文件
+	git config --global user.name "youjiahe"
+	git config --global user.email "youjiahe@163.com"
+	git config --global core.editor vim
+	git config --list
+	git init
+	git add <files> /git add . /git add --all
+	git status .
+	git commit -m "first_commit"
+	git commit -am "first_commit"   #先add，再commit
+	git ls-files
+	git rm <files>  #删除版本库文件
+	git remote rename origin old-origin
+	git remote remove origin
+	git remote add origin git@192.168.4.1:nsd1806/core_py.git
+	git push -u origin --all
+	git clone git@192.168.4.1:nsd1806/core_py.git
+	git pull
+	git reset HEAD hello.py    #撤出暂存区 
+	git checkout -- hello.py   #撤出后恢复文件
 
 ##############################################################################
 git 客户端使用
@@ -120,19 +120,19 @@ git 客户端使用
    ssh-add  
 ##############################################################################
 ● git config -global
-[root@room9pc01 ~]# git config --global user.name "youjiahe"
-[root@room9pc01 ~]# git config --global user.email "youjiahe@163.com"
-[root@room9pc01 ~]# git config --global core.editor vim
-[root@room9pc01 ~]# git config --list
-user.name=youjiahe
-user.email=youjiahe@163.com
-core.editor=vim
-[root@room9pc01 tedu]# cat ~/.gitconfig
-[user]
-	name = youjiahe
-	email = youjiahe@163.com
-[core]
-	editor = vim
+	[root@room9pc01 ~]# git config --global user.name "youjiahe"
+	[root@room9pc01 ~]# git config --global user.email "youjiahe@163.com"
+	[root@room9pc01 ~]# git config --global core.editor vim
+	[root@room9pc01 ~]# git config --list
+	user.name=youjiahe
+	user.email=youjiahe@163.com
+	core.editor=vim
+	[root@room9pc01 tedu]# cat ~/.gitconfig
+	[user]
+		name = youjiahe
+		email = youjiahe@163.com
+	[core]
+		editor = vim
 	
 ##############################################################################
 git
@@ -146,78 +146,78 @@ git
 ##############################################################################
 ● git使用
 & 创建自己的版本库
-[root@room9pc01 python]# mkdir youjiahe
-[root@room9pc01 python]# cd youjiahe
-[root@room9pc01 py01]# git init
-初始化空的 Git 版本库于 /root/tedu/youjiahe/.git/
+	[root@room9pc01 python]# mkdir youjiahe
+	[root@room9pc01 python]# cd youjiahe
+	[root@room9pc01 py01]# git init
+	初始化空的 Git 版本库于 /root/tedu/youjiahe/.git/
 
 & 创建脚本并且添加到暂存区
-[root@room9pc01 py01]# echo "print('hello world')" > hello.py
-[root@room9pc01 py01]# git add hello.py  #git add --all/ git add .
-[root@room9pc01 py01]# git status .
-[root@room9pc01 py01]# echo "print('welcome')" > welcome.py
-[root@room9pc01 py01]# ls
-hello.py  welcome.py
-[root@room9pc01 py01]# git add .
-[root@room9pc01 py01]# git status -s  # A-新增文件  D-本地删除文件
-A  hello.py
-A  welcome.py  
+	[root@room9pc01 py01]# echo "print('hello world')" > hello.py
+	[root@room9pc01 py01]# git add hello.py  #git add --all/ git add .
+	[root@room9pc01 py01]# git status .
+	[root@room9pc01 py01]# echo "print('welcome')" > welcome.py
+	[root@room9pc01 py01]# ls
+	hello.py  welcome.py
+	[root@room9pc01 py01]# git add .
+	[root@room9pc01 py01]# git status -s  # A-新增文件  D-本地删除文件
+	A  hello.py
+	A  welcome.py  
 
 & 把暂存区的文件提交到版本库中
-[root@room9pc01 py01]# git commit -m "first_commit"
-[master（根提交） ae8ae41] first_commit
- 2 files changed, 2 insertions(+)
- create mode 100644 hello.py
- create mode 100644 welcome.py
+	[root@room9pc01 py01]# git commit -m "first_commit"
+	[master（根提交） ae8ae41] first_commit
+	 2 files changed, 2 insertions(+)
+	 create mode 100644 hello.py
+	 create mode 100644 welcome.py
 
 & 更新文件提交到版本库
-[root@room9pc01 py01]# echo "print('Python day1')" > hello.py
-[root@room9pc01 py01]# git commit -am 'first_commit'
-[master 39d8558] first_commit
- 1 file changed, 1 insertion(+), 1 deletion(-)
+	[root@room9pc01 py01]# echo "print('Python day1')" > hello.py
+	[root@room9pc01 py01]# git commit -am 'first_commit'
+	[master 39d8558] first_commit
+	 1 file changed, 1 insertion(+), 1 deletion(-)
 
 & 查看版本库文件
-[root@room9pc01 py01]# git ls-files
-hello.py
-welcome.py
+	[root@room9pc01 py01]# git ls-files
+	hello.py
+	welcome.py
 
 & 删除版本库文件
-[root@room9pc01 py01]# git rm welcome.py
-[root@room9pc01 py01]# git status
-[root@room9pc01 py01]# git commit -m "rm test file"
-[root@room9pc01 py01]# git ls-files
+	[root@room9pc01 py01]# git rm welcome.py
+	[root@room9pc01 py01]# git status
+	[root@room9pc01 py01]# git commit -m "rm test file"
+	[root@room9pc01 py01]# git ls-files
 hello.py
 
 ##############################################################################
 搭建gitlab 服务端
 环境准备
 ● 准备一台虚拟机 4G内存 40G硬盘
-& 装包
-[root@python_git docker_pkgs]# ls  #老师提供
-[root@python_git docker_pkgs]# yum -y install *.rpm
+  & 装包
+	[root@python_git docker_pkgs]# ls  #老师提供
+	[root@python_git docker_pkgs]# yum -y install *.rpm
 
-& 导入镜像
-[root@python_git images]# systemctl restart docker
-[root@python_git images]# docker load < gitlab_zh.tar
-[root@python_git images]# docker images
+  & 导入镜像
+	[root@python_git images]# systemctl restart docker
+	[root@python_git images]# docker load < gitlab_zh.tar
+	[root@python_git images]# docker images
 
-&修改ssh端口，非22就行
-[root@python_git ~]# vim /etc/ssh/sshd_config #修改22为2222
-[root@python_git ~]# systemctl restart sshd
+  &修改ssh端口，非22就行
+	[root@python_git ~]# vim /etc/ssh/sshd_config #修改22为2222
+	[root@python_git ~]# systemctl restart sshd
 ##############################################################################
 Gitlab启动
 ●将docker主机ssh端口改为2222后,起动容器
-[root@python_git ~]# docker run -d -h gitlab \
---name gitlab \
--p 443:443 -p 80:80 -p 22:22 \
---restart always \
--v /srv/gitlab/config:/etc/gitlab \
--v /srv/gitlab/logs:/var/log/gitlab \
--v /srv/gitlab/data gitlab_zh:latest
+	[root@python_git ~]# docker run -d -h gitlab \
+	--name gitlab \
+	-p 443:443 -p 80:80 -p 22:22 \
+	--restart always \
+	-v /srv/gitlab/config:/etc/gitlab \
+	-v /srv/gitlab/logs:/var/log/gitlab \
+	-v /srv/gitlab/data gitlab_zh:latest
 
-//-h:主机名
-//--name gitlab:UUID
-//--restart always:容器故障自动充气
+	//-h:主机名
+	//--name gitlab:UUID
+	//--restart always:容器故障自动充气
 
 ●浏览器浏览虚拟机IP
 firefox 192.168.4.1  #会看到"GitLab 中文社区版"
@@ -227,14 +227,14 @@ firefox 192.168.4.1  #会看到"GitLab 中文社区版"
 git
 推送文件到远程  #有密码
 ●创建远程仓库
-[root@room9pc01 youjiahe]# git remote rename origin old-origin 
-error: 不能重命名配置小节 'remote.origin' 到 'remote.old-origin'  #不用管
-[root@room9pc01 youjiahe]# git remote add origin http://192.168.4.1/nsd1806/core_py.git
+	[root@room9pc01 youjiahe]# git remote rename origin old-origin 
+	error: 不能重命名配置小节 'remote.origin' 到 'remote.old-origin'  #不用管
+	[root@room9pc01 youjiahe]# git remote add origin http://192.168.4.1/nsd1806/core_py.git
  
 ●往刚刚创建的项目中推送文件
-[root@room9pc01 youjiahe]# git push -u origin --all
-Username for 'http://192.168.4.1': root                           do
-Password for 'http://root@192.168.4.1':                          do
+	[root@room9pc01 youjiahe]# git push -u origin --all
+	Username for 'http://192.168.4.1': root                           do
+	Password for 'http://root@192.168.4.1':                          do
 
 ●回到浏览器项目页面查看内容
  可以看到仓库文件
@@ -245,29 +245,29 @@ git
  粘贴密钥到设置----ssh密钥
 
 ●回到真机
-[root@room9pc01 youjiahe]# ssh-agent   #启动ssh代理程序
-[root@room9pc01 youjiahe]# ssh-add     #添加私钥，让代理程序托管
+	[root@room9pc01 youjiahe]# ssh-agent   #启动ssh代理程序
+	[root@room9pc01 youjiahe]# ssh-add     #添加私钥，让代理程序托管
 
-//ssh-agent 用于管理 ssh private keys，目的是对解密的私钥进行高速缓存。 
-//ssh-add 提示并将用户使用的私钥添加到 ssh-agent 维护列表中，此后当公钥连接到远程 SSH 或 SCP 主机时，不再提示信息。
-//ssh-add 命令是把专用密钥添加到ssh-agent的高速缓存中。该命令位置在/usr/bin/ssh-add。
+	//ssh-agent 用于管理 ssh private keys，目的是对解密的私钥进行高速缓存。 
+	//ssh-add 提示并将用户使用的私钥添加到 ssh-agent 维护列表中，此后当公钥连接到远程 SSH 或 SCP 主机时，不再提示信息。
+	//ssh-add 命令是把专用密钥添加到ssh-agent的高速缓存中。该命令位置在/usr/bin/ssh-add。
 
 ●管理区域(小扳手)----project-----选一个项目
    可以看到多了 ssh一行
 
 ●无密码方式推送文件
-[root@room9pc01 youjiahe]# git remote remove origin #
-[root@room9pc01 youjiahe]# git remote add origin git@192.168.4.1:nsd1806/core_py.git
-[root@room9pc01 youjiahe]# cp /etc/hosts .
-[root@room9pc01 youjiahe]# git add .
-[root@room9pc01 youjiahe]# git status
-[root@room9pc01 youjiahe]# git commit -m "add files"
-[root@room9pc01 youjiahe]# git push origin master
+	[root@room9pc01 youjiahe]# git remote remove origin #
+	[root@room9pc01 youjiahe]# git remote add origin git@192.168.4.1:nsd1806/core_py.git
+	[root@room9pc01 youjiahe]# cp /etc/hosts .
+	[root@room9pc01 youjiahe]# git add .
+	[root@room9pc01 youjiahe]# git status
+	[root@room9pc01 youjiahe]# git commit -m "add files"
+	[root@room9pc01 youjiahe]# git push origin master
 ##############################################################################
 git
 ●从远程仓库下载本机
-[root@room9pc01 tedu]# rm -rf youjiahe/
-[root@room9pc01 tedu]# git clone git@192.168.4.1:nsd1806/core_py.git
+	[root@room9pc01 tedu]# rm -rf youjiahe/
+	[root@room9pc01 tedu]# git clone git@192.168.4.1:nsd1806/core_py.git
 ##############################################################################
 Python运行方式
 ● 解释器
@@ -290,22 +290,22 @@ Python运行方式
     hello world    
 ############################################################################## 
 Python语法结构
-1.语法块缩进
-  –  4个空格:非常流行, 范·罗萨姆支持的风格  #可读性
-2.注释及续行
-  #   #“#”为注释
-  \   #续行符，解释时视为同一行
-3.同行多个语句
-  ；  #用“；”分隔
+	1.语法块缩进
+	  –  4个空格:非常流行, 范·罗萨姆支持的风格  #可读性
+	2.注释及续行
+	  #   #“#”为注释
+	  \   #续行符，解释时视为同一行
+	3.同行多个语句
+	  ；  #用“；”分隔
 ############################################################################## 
 ● print语句
-print(values,...,sep=' ',end='\n')
-//sep是两个值间的值
-//end是最后一个值后添加的字符串
+	print(values,...,sep=' ',end='\n')
+	//sep是两个值间的值
+	//end是最后一个值后添加的字符串
 
 ● input语句
-//作用：提示字符串；在提示字符串后写要输入的内容
-//可以使用变量接收内容
+	//作用：提示字符串；在提示字符串后写要输入的内容
+	//可以使用变量接收内容
 ##############################################################################
 案例5:模拟用户登陆
 1.  创建名为login.py的程序文件
--- 15.Python/Py3.sh
+++ 15.Python/Py3.sh
@@ -4,9 +4,14 @@ Python自动化运维 day03
    1.2 range函数
    1.3 列表解析
 2.文件对象
-3.函数基础
+   2.1 文件打开关闭方法  open/close/with
+   2.2 文件输入          read/readline/readlines
+   2.3 文件输出          write/writelines
+   2.4 操作文件          seek/tell
+3.函数基础   def
 4.模块基础
-
+  4.1 定义模块
+  4.2 使用模块
 ##############################################################################
 ●批量缩进
    选中---->TAB
@@ -28,98 +33,99 @@ for 变量列表 in 可迭代对象:
   — 如果使用for循环遍历一个空的可迭代对象，循环不会执行，对应的变量也可能不会创建
   — 数字不是可迭代对象； not iterrable ---->不可迭代
 ●例子 
->>> for ch in 'abcd':
-...     print(ch)
-... 
-a
-b
-c
-d
+	>>> for ch in 'abcd':
+	...     print(ch)
+	... 
+	a
+	b
+	c
+	d
 
->>> for i in [1,2,3,4,5]:  #数字的迭代使用列表/元组
-...     print(int(i))
-... 
-1
-2
-3
-4
-5    
->>> for i in 100:       #数字不可迭代
-...     print(i)
-... 
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-TypeError: 'int' object is not iterable
+	>>> for i in [1,2,3,4,5]:  #数字的迭代使用列表/元组
+	...     print(int(i))
+	... 
+	1
+	2
+	3
+	4
+	5    
+	>>> for i in 100:       #数字不可迭代
+	...     print(i)
+	... 
+	Traceback (most recent call last):
+	  File "<stdin>", line 1, in <module>
+	TypeError: 'int' object is not iterable
 ##############################################################################
 range函数
-range(start,end)和切片相同，不包含结束值
-返回以和可迭代的整数序列，通常和for循环搭配使用
+	range(start,end)和切片相同，不包含结束值
+	返回以和可迭代的整数序列，通常和for循环搭配使用
 ##############################################################################
 for循环  range函数
 ●案例1：
    打印数字1～20
->>> for i in range(1,21):
-...     print(i,end=' ')
-... else:
-...     print('')
-... 
-1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 
->>> 
+	>>> for i in range(1,21):
+	...     print(i,end=' ')
+	... else:
+	...     print('')
+	... 
+	1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 
+	>>> 
 ##############################################################################
 for循环  方法：列表.append 
 #!/usr/bin/env python3
 ●案例2：斐波那契数列 ----- 最后一个数总是前两个书之和
-#初始化列表 fibs=[0,1]
-#fibs[0,1,1]
-#fibs[0,1,1,2]
-#fibs[0,1,1,2,3]
-#使用append方法进行添加   列表名.append(列表值)
+	#初始化列表 fibs=[0,1]
+	#fibs[0,1,1]
+	#fibs[0,1,1,2]
+	#fibs[0,1,1,2,3]
+	#使用append方法进行添加   列表名.append(列表值)
 
-fibs=[0,1]
-for i in range(10):  #追加10个元素
-    fibs.append(fibs[-2]+fibs[-1])
-print(fibs)
+	fibs=[0,1]
+	for i in range(10):  #追加10个元素
+		fibs.append(fibs[-2]+fibs[-1])
+		
+	print(fibs)
 
-结果：
-[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+	结果：
+	[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 ##############################################################################
 for循环
 ●案例3： 99乘法表
 &打印方法1：  print拼接
-#/usr/bin/env python3
-end=9
-for i in range(1,end+1):
-    for j in range(1,i+1):
-        if i*j<10:
-            print(j, '×', i, '=', i * j, sep='', end='  ')
-         
-        else:
-            print(j,'×',i,'=',i*j,sep='',end=' ')
-    print('')
-    
+	#/usr/bin/env python3
+	end=9
+	for i in range(1,end+1):
+		for j in range(1,i+1):
+		    if i*j<10:
+		        print(j, '×', i, '=', i * j, sep='', end='  ')
+		     
+		    else:
+		        print(j,'×',i,'=',i*j,sep='',end=' ')
+		print('')
+		
 &打印方法2：  用结构化字符串打印    '%d×%d=%d' % (j,i,i*j)
-#/usr/bin/env python3
-end=9
-for i in range(1,end+1):
-    for j in range(1,i+1):
-        if i*j<10:
-            print('%d×%d=%d' % (j,i,i*j),end='  ')
-#            print(j, '×', i, '=', i * j, sep='', end='  ')
-        else:
-            print('%d×%d=%d' % (j,i,i*j),end=' ')
-#            print(j,'×',i,'=',i*j,sep='',end=' ')
-    print('')
+	#/usr/bin/env python3
+	end=9
+	for i in range(1,end+1):
+		for j in range(1,i+1):
+		    if i*j<10:
+		        print('%d×%d=%d' % (j,i,i*j),end='  ')
+	#            print(j, '×', i, '=', i * j, sep='', end='  ')
+		    else:
+		        print('%d×%d=%d' % (j,i,i*j),end=' ')
+	#            print(j,'×',i,'=#',i*j,sep='',end=' ')
+		print('')
 
-1×1=1  
-1×2=2  2×2=4  
-1×3=3  2×3=6  3×3=9  
-1×4=4  2×4=8  3×4=12 4×4=16 
-1×5=5  2×5=10 3×5=15 4×5=20 5×5=25 
-1×6=6  2×6=12 3×6=18 4×6=24 5×6=30 6×6=36 
-1×7=7  2×7=14 3×7=21 4×7=28 5×7=35 6×7=42 7×7=49 
-1×8=8  2×8=16 3×8=24 4×8=32 5×8=40 6×8=48 7×8=56 8×8=64 
-1×9=9  2×9=18 3×9=27 4×9=36 5×9=45 6×9=54 7×9=63 8×9=72 9×9=81
+	1×1=1  
+	1×2=2  2×2=4  
+	1×3=3  2×3=6  3×3=9  
+	1×4=4  2×4=8  3×4=12 4×4=16 
+	1×5=5  2×5=10 3×5=15 4×5=20 5×5=25 
+	1×6=6  2×6=12 3×6=18 4×6=24 5×6=30 6×6=36 
+	1×7=7  2×7=14 3×7=21 4×7=28 5×7=35 6×7=42 7×7=49 
+	1×8=8  2×8=16 3×8=24 4×8=32 5×8=40 6×8=48 7×8=56 8×8=64 
+	1×9=9  2×9=18 3×9=27 4×9=36 5×9=45 6×9=54 7×9=63 8×9=72 9×9=81
 ##############################################################################
 for循环
 ●列表解析
@@ -128,20 +134,379 @@ for循环
 
 
 ●案例4：把1～20放到列表里面
-& 方法一:
->>> for i in range(1,21):
-...     alist.append(i)
-... 
->>> alist
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+	& 方法一:
+	>>> for i in range(1,21):
+	...     alist.append(i)
+	... 
+	>>> alist
+	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-& 方法二：
->>> [i for i in range(1,21)]
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+	& 方法二：
+	>>> [i for i in range(1,21)]
+	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-& 打印偶数：
->>> [ i for i in range(1,21) if i%2==0]
-[2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+	& 打印偶数：
+	>>> [ i for i in range(1,21) if i%2==0]
+	[2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+##############################################################################
+文件对象
+文件打开方法
+●open()内键函数
+  & 基本语法:
+   file_object = open(file_name, access_mode='r', buffering=-1)	
+   //access_mode:文件访问模式
+    //文件访问模式：
+    r  读      文件不存在报错  #默认
+    w  写      文件不存在创建文件，存在则先清空再写入
+    a  追加    文件不存在创建文件，像文件末尾写入内容
+     +  读写模式
+    b  二进制模式
+  &例子
+  —  打开和关闭文件
+  >>> files=open('/root/git/sh/host.txt','r')
+  >>> files.close()
+  >>> files
+  <_io.TextIOWrapper name='/root/git/sh/host.txt' mode='r' encoding='UTF-8'>
+##############################################################################
+文件对象
+文件输入
+●read()
+ & read(size)
+    — size代表字节数、默认读取全部文件内容,作为一个字符串返回
+    — size默认-1
+ & 注意事项;
+    — 打开文件后会有一个指针，指针会根据用户操作移动，读写通常是针对指针后面的文件内容
+    — 如果读取全部文件内容，指针会移动到文件的最后，这是再读取文件，指针后没有内容，返回空字符串
+    
+ & 例子：
+  >>> files=open('/root/git/sh/host.txt')
+  >>> files.read()
+  '192.168.1.11\n192.168.1.12\n192.168.1.13\n192.168.1.14\n192.168.1.15\n192.168.1.16\n192.168.1.20\n'
+  >>> files.read()
+  ''                        #因为读取了一编、指针位置移动到最后面，内容为空，因此结果是空
+  >>> files.close()
+
+
+●readline()
+ & readline(size)
+    — size代表字节数、默认读取整行
+    — size默认-1
+
+●readlines()
+ & readlines()
+    — 读取所有行(剩余的),把它们作为一个列表返回
+    — 指定数字，则读取一行
+    
+ &例子：  readline、readlines
+  [root@room9pc01 ~]# cp /etc/passwd /opt/passwd
+  [root@room9pc01 ~]# python3
+  >>> passwd=open('/opt/passwd')
+  >>> passwd.readline()                  #读1行
+  'root:x:0:0:root:/root:/bin/bash\n'
+  >>> passwd.readline(5)                 #读取5个字符
+  'bin:x'
+  >>> passwd.readlines()                 #剩余的所有行，与read有区别，read是整一篇文档内容
+  [':1:1:bin:/bin:/sbin/nologin\n', ......
+  ........ ..'.........................\n']        
+
+##############################################################################
+文件输出
+write方法
+●write()
+  — 文件对象的访问模式需要支持写入
+  — 默认情况下文件的写入不会立即生效，需要文件对象关闭后生效
+  — 写入文件时,不会自动添加行结束标志,需要程序员手工输入
+  
+●writelines()
+  — 和readlines()一样,writelines()方法是针对列表的操作
+  
+   [root@room9pc01 ~]# cat python_test  
+	#!/usr/bin/env python3
+	test1
+	test2
+	test3
+	test4
+	test5
+	test6
+	[root@room9pc01 ~]# python3
+	>>> test=open('python_test')
+	>>> test.write('test10\n')
+	7
+	>>> test.writelines('test11\n')
+	>>> data=['test12\n','test13']    #以列表的方式写入
+	>>> test.writelines(data)
+	>>> test.close()                  #与属性test.closed区别开来
+	[root@room9pc01 ~]# cat python_test 
+	#!/usr/bin/env python3
+	test1
+	test2
+	test3
+	test4
+	test5
+	test6
+	test10
+	test11
+	test12
+	test13
+##############################################################################
+操作文件
+with子句
+●with语句是用来简化代码的
+  & 在将打开文件的操作放在with语句中,代码块结束后,
+     文件将自动关闭
+     
+  & 例子：
+	>>> with open('1.txt','w') as f:
+	...     data=['line1\n','line2\n']
+	...     f.writelines(data)
+	... 
+	>>> f.closed
+	True
+	>>> f=open('1.txt')
+	>>> f.readlines()
+	['line1\n', 'line2\n']
+##############################################################################
+操作文件
+文件内移动
+●seek()
+  & seek(offset[,whence])
+      — 移动文件指针到不同的位置
+     — offset是相对于某个位置的偏移量,也就是代表需要移动偏移的字节数。
+     — whence，0-开头，1-当前位置，2-文件的结尾
+●tell()
+ & tell()
+      — 	返回当前文件指针的位置
+      
+      
+  & 例子：
+	>>> with open('1.txt','rb') as f:    #必须以二进制方式打开文件，seek才有效
+	...     f.seek(-20,2)  #移动到文档最后，再往前20个字符
+	...     f.read()       #读取文档最后20个字符
+	...     f.seek(3)      #直接移动到指针位置3
+	...     f.tell()
+	11
+	b'\nline3\nline4\nline5\n\n'
+	3
+    3
+##############################################################################
+●文件对象是可迭代的
+	>>> f=open('1.txt')
+	>>> for line in f:
+	...     print(line)
+	... 
+	line1
+
+	line2
+
+	line3
+
+	line4
+
+	line5
+
+	line6
+##############################################################################
+●案例7:模拟cp操作
+	1.  创建cp.py文件
+	2.  将/bin/ls“拷贝”到/root/目录下
+	3.  不要修改原始文件
+	
+	#!/usr/bin/env python3
+	src_obj = open('/bin/ls','rb')    #以二进制的方式打开文件，不会有编码问题
+	dst_orj = open('/root/ls','wb')
+
+	while True:
+		data=src_obj.read(4096)   #只读取4k数据
+		if not data:
+		    break
+		dst_obj.write(data)
+
+	src_obj.close()
+	dst_obj.close()
+##############################################################################
+函数基础
+●python中的函数
+  — 函数一般是实现某种功能的代码
+  — 作用：1.减少代码额的重复使用；
+
+●函数语法格式:
+def 函数名:(参数列表)
+    '文档字符串'
+    fun_suit
+    
+●函数注意事项：
+   & 函数名要求同变量名
+   & 写在函数内的变量，在调用之后会被释放 
+   
+●return
+ — 返回一个结果
+ — 函数中如果没有return语句，默认在结尾添加"return None"
+
+●例子：
+1.hello world!
+	----------------------------------------
+	#!/usr/bin/env python3
+	def hello():
+		print('hello world!')
+	hello()                      #调用函数
+	print(hello)
+	----------------------------------------
+
+	输出结果：
+	hello world!                 #调用函数结果
+	<function hello at 0x7f71a4fcaea0>   #该函数在内存的位置
+
+2.return
+	#!/usr/bin/env python3
+	def fun():
+		res = 3+4
+		return res     #在调用函数的地方返回一个结果
+	print(fun())      #相当于print(7)
+	print(fun()*2)    #相当于print(7*2)
+
+    输出结果：
+	7
+	14
+
+2.1 return None
+	#!/usr/bin/env python3
+	def fun():
+		res=10000+5000
+	print(fun())
+
+	输出结果：
+	None
+##############################################################################
+函数基础
+函数中的参数
+形参、实参
+●函数中的参数
+  参数是函数运行时必须要的值
+  
+●参数分为两种：形式参数(形参) 和 实际参数(实参)
+  — 形参是定义函数是写在()中的参数
+  — 实参是条用函数时写在()中的参数
+
+●例子: 
+  &斐波那契数列函数  #传参，传一个
+	#!/usr/bin/env python3	
+	def fib(num):
+		fibs=[0,1]
+		for i in range(num-2):  #追加10个元素
+		    fibs.append(fibs[-2]+fibs[-1])
+		print(fibs)
+	fib(4)
+	fib(15)
+	
+  & 拷贝文件		
+	#!/usr/bin/env python3
+	def copy(src,dst):
+		src_obj = open(src,'rb')   #以二进制的方式打开文件，不会有编码问题
+		dst_obj = open(dst,'wb')
+		while True:
+		    data=src_obj.read(4096)   #只读取4k数据
+		    if not data:
+		        break
+		    dst_obj.write(data)
+
+		src_obj.close()
+		dst_obj.close()
+
+	copy('/bin/ls','/opt/ls1')
+	copy('/bin/ls','/opt/ls2')
+##############################################################################
+函数中的参数  sys.argv  sys模块argv列表
+●位置参数
+  — 与shell脚本类似,程序名以及参数都以位置参数的方式
+     传递给python程序
+  — 使用sys模块的argv列表接收
+	
+●例子：
+  & 把位置变量打印出来，以列表的方式
+  -----------------------------------------------
+	#!/usr/bin/env python3
+	import sys
+	print(sys.argv)
+  ------------------------------------------------
+	[root@room9pc01 python3]# python3 sys_argv.py you jia he
+	['sys_argv.py', 'you', 'jia', 'he']
+
+  & 拷贝文件
+  ----------------------------------------------------------------
+	#!/usr/bin/env python3
+	import  sys
+	def copy():
+		src_obj = open(sys.argv[1],'rb')   #以二进制的方式打开文件，不会有编码问题
+		dst_obj = open(sys.argv[2],'wb')
+
+		while True:
+		    data=src_obj.read(4096)   #只读取4k数据
+		    if not data:
+		        break
+		    dst_obj.write(data)
+
+		src_obj.close()
+		dst_obj.close()
+	copy()
+   ----------------------------------------------------------------
+	[root@room9pc01 python3]# python3 fun_argv_copy.py /etc/passwd /python/passwd
+	[root@room9pc01 python3]# ls /python/passwd 
+	/python/passwd
+	[root@room9pc01 python3]# ll /python/passwd 
+	-rw-r--r-- 1 root root 2180 11月  9 17:32 /python/passwd
+##############################################################################
+函数基础
+函数中的参数
+●默认参数
+  默认参数就是声明了默认值的参数
+●例子：
+  & 打印*号，默认打印30个
+	#!/usr/bin/env python3
+	def pstar(num=30):
+		print('*'*num)
+	pstar()
+	pstar(5)
+   
+	输出结果:
+	******************************
+	*****
+##############################################################################
+模块基础
+定义模块
+●模块基本概念
+  — 模块是从逻辑上组织python代码的形式
+●创建模块注意事项
+  — 模块名称以 .py结尾
+  — 模块名称不要与系统中存在的重名
+  — 模块文件名字去掉后面的(.py)即为模块名
+
+●导入模块(import)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
