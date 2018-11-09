13e98496c09ee06bb96c94409954e0a2b2505c7c
youjiahe <youjiahe@163.com>
2018-11-09 13:14:01 +0800

    update_2018-11-09_13:14

--- git_co.sh
+++ git_co.sh
@@ -4,7 +4,7 @@ ssh-add /root/.ssh/github/id_rsa
 for i in  /root/git/{sh,note,python} 
 do
    cd $i
-   git pull
+   git pull origin master
    git add --all
    git commit -m "update_`date +%F_%R`"
    git push origin master
