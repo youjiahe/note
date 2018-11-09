98e06768e4916e2c1ec6d6c79057f2423dabc1cf
youjiahe <youjiahe@163.com>
2018-11-09 13:10:48 +0800

    update_2018-11-09_13:10

--- git_co.sh
+++ git_co.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 eval $(ssh-agent -s)
 ssh-add /root/.ssh/github/id_rsa
-cd /root/git/note
-git add --all
-git commit -m "$h_`date +%F_%R`"
-git push origin master
-cd /root/git/sh
-git add --all
-git commit -m "$h_`date +%F_%R`"
-git push origin master
-cd /root/git/python
-git add --all
-git commit -m "$h_`date +%F_%R`"
-git push origin master
+for i in  /root/git/{sh,note,python} 
+do
+   cd $i
+   git pull
+   git add --all
+   git commit -m "update_`date +%F_%R`"
+   git push origin master
+done
