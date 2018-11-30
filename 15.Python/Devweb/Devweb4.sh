93eb76dbdf0846bba3e56fc504510e31593ee44b
youjiahe <youjiahe@163.com>
2018-11-30 18:00:25 +0800

    update_2018-11-30_18:00

--- 15.Python/Devweb/Devweb4.sh
+++ 15.Python/Devweb/Devweb4.sh
@@ -161,14 +161,14 @@ Django简介
    — 一个项目有很多的功能，可以将这些功能编写成一个个的应用。每个应用都是可重用的
    	  如果其他项目也需要有完全一样的功能，只要加载这个应用即可。
    	  
-   (djenv) [root@room9pc01 mysite]# python manage.py startproject polls
+   (djenv) [root@room9pc01 mysite]# python manage.py startapp poll
 
 ●  激活应用——即将应用安装到项目中
 		创建应用后，应用不会自动成为项目的一员。需要修改配置文件
 	# vim mysite/settings.py
 	INSTALLED_APPS = [
 		... ...
-		'polls',
+		'poll',
 	]
 
 ●   
@@ -187,10 +187,191 @@ Django简介
 	11、创建应用，并将应用加入到项目中
 
 
+  598  python3 -m venv /opt/pro1
+  599  source /opt/pro1/bin/activate
+  600  pip3 install pymysql django
+  601  ls
+  602  cd git/python/devweb/day4/
+  603  ls
+  604  django-admin startproject pro1
+  605  ls
+  606  cd ls
+  607  cd pro1/
+  608  ls
+  609  vim pro1/__init__.py 
+  610  mysql -uroot -p123456
+  611  vim pro1/settings.py 
+  612  python manage.py migrate
+  613  vim pro1/__init__.py 
+  614  python manage.py migrate
+  615  vim pro1/settings.py 
+  616  python manage.py migrate
+  617  python manage.py createsuperuser
+  618  python manage.py runserver 0:80
+  619  history 
+##############################################################################
+● 编辑应用
+   & 创建投票应用
+   1.投票应用需要在数据库中创建两张表
+      问题表： 问题编号、内容
+      选项表：选项编号、选项内容、问题编号、所的票数
+   2.URL设计
+      http://127.0.0.1/poll           ->列出所有问题
+      http://127.0.0.1/poll/1         ->列出1号问题详细信息
+      http://127.0.0.1/poll/1/result  ->显示1号问题的投票结果
+   3.模板设计
+   		首页(列出所有问题的页面)：index.html
+   		问题详情页： default.html
+   		投票结果页： result.html
+##############################################################################   		
+● 编辑应用 
+   4.配置 URLConf 
+      将 以 http://127.0.0.1/poll/ 开头的网址都交给poll应用处理 
+   # vim mysite/urls.py
+   	from django.conf.urls import url,include
+	from django.contrib import admin
 
+	urlpatterns = [
+	    url(r'^admin/', admin.site.urls),
+	    url(r'^poll/',include('poll.urls') )
+	]
+   
+   5. 配置poll/urls 处理请求
+   # vim poll/urls.py
+   from django.conf.urls import url
+	from . import views
+	urlpatterns=[
+	]
+##############################################################################
+● 编辑应用
+  6. 创建首页文件
+ 	(1)在poll/urls.py中创建url和函数的声明
+	  # vim poll/urls.py
+	   from django.conf.urls import url
+		from . import views
+		urlpatterns=[
+		   #将url为 http://127.0.0.1/poll 的请求都转发到 index 函数处理， 这个url叫 index
+			url('^$', view.index, name='index')
+		]  
+   (2)修改 poll/view.py 创建简单首页 测试视图  
+	  from django.shortcuts import render, HttpResponse
+	  def index(request):
+   		   return HttpResponse('<h1>poll首页</h1>')
+   
+   (3)访问http://127.0.0.1/poll  #
+   (djenv) [root@room9pc01 mysite]# python manage.py runserver 0:80
+	[root@room9pc01 mysite]#  firefox http://127.0.0.1/poll
+	
+   (4)创建模板，用户访问主页的时候，用index.html响应 
+		(djenv) [root@room8pc16 mysite]# mkdir poll/templates
+		# vim poll/templates/index.html
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>投标首页</title>
+		</head>
+		<body>
+		这是投票首页
+		</body>
+		</html>
+	(5)修改index函数，将index.html网页返回给用户
+	# vim poll/views.py
+	def index(request):
+		return render(request, 'index.html')
+##############################################################################
+● 编辑应用
+  7.创建模型 (数据库)
+   (1) 编写模型代码
+   #vim  poll/models.py
+   from django.db import models
+	from . import views
+	# Create your models here.
+	class Questions(models.Model):
+		question_text=models.CharField(max_length=200)
+		pub_date = models.DateTimeField()
 
+		def __str__(self):
+		    return '<问题： %s>' % self.question_text
 
+	class Choice(models.Model):
+		choice_text = models.CharField(max_length=200)
+		vote = models.IntegerField()
+		question = models.ForeignKey(Questions,on_delete=models.CASCADE)
 
+		def __str__(self):
+		    return '<%s: %s>' % (self.question,self.choice_text)
+		    
+    (2)生成数据表
+       & 生成修改数据库的脚本文件  
+       (djenv) [root@room8pc16 mysite]# python manage.py makemigrations
+       & 生成数据表
+       (djenv) [root@room8pc16 mysite]# python manage.py migrate
+			MariaDB [dj1806]> show tables;   # 查看到新出现了两张表
+##############################################################################	
+● 编辑应用
+	8.	分析数据库
+		(1)数据表
+		MariaDB [dj1806]> show tables;
+			poll_question      poll_choice
+			通过第7步生成的表名： 应用名_类名
+		(2)poll_question的字段
+			MariaDB [dj1806]> desc poll_question;
+			id: int            # -> django自动生成的主键
+			question_text: varchar(200)   # 类属性
+			pub_date: datetime            # 类属性
+		(3)poll_choice的字段
+			MariaDB [dj1806]> desc poll_choice;
+			id: int  # ->django自动生成的主键
+			choice_text: varchar(200)     # 类属性
+			vote: int      # 类属性
+			q_id: int      # django根据主外键约束，自动生成的外键字段
+			
+    9. 修改数据库字段
+      # vim poll/models.py
+       class Choice(models.Model):
+			... ...
+			question = models.ForeignKey(Question, on_delete=models.CASCADE)
+				def __str__(self):
+				return "<%s: %s>" % (self.question, self.choice_text)
+
+		(djenv) [root@room8pc16 mysite]# python manage.py makemigrations
+		(djenv) [root@room8pc16 mysite]# python manage.py migrate
+		MariaDB [dj1806]> desc polls_choice;
+		原来的q_id变成了question_id
+##############################################################################
+● 编辑应用
+  10. 注册表格
+  将模型注册到管理后台
+	# vim poll/admin.py
+	from django.contrib import admin
+	from .models import Question, Choice
+
+	admin.site.register(Question)
+	admin.site.register(Choice)
+##############################################################################
+● 编辑应用
+  11 定制后台管理
+  # vim poll/admin.py 
+  from django.contrib import admin
+  from .models import Question, Choice
+
+  # admin.site.register(Question)
+  class QuestionAdmin(admin,ModelAdmin):
+      # 后台显示那些字段
+      list_display=['question_text','pub_date']
+      # 根据日期过滤问题
+      list_filter=['pub_date']
+      # 显示时间轴、点选时间、查看某年、某月、某日的问题
+      date_hierarchy='pub_date'
+      # 添加搜索框， 根据问题内容进行搜索
+      search_fields=['question_text']
+      # 根据发布时间进行降序排序，没有‘-’号，默认时升序
+      ordering = ['-pub-date']
+  admin.site.register(QuestionAdmin,Question)
+  admin.site.register(Choice)
+      
+##############################################################################
 
 
 
