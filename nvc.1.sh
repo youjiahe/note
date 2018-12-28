3c9f83d8e97fcbed65bd9abbd4357f32558d001e
youjiahe <youjiahe@163.com>
2018-12-28 12:00:03 +0800

    update_2018-12-28_12:00

--- nvc.1.sh
+++ nvc.1.sh
@@ -3,6 +3,7 @@ if [ -z $1 ]||[ -z $2 ]||[ -z $3 ];then
    echo "nvc.sh <虚拟机名> <主机名> <eth0 IP> <eth1 IP> <eth2 IP> <eth3 IP>" >&2
    exit 1
 fi
+cd /root/git/sh
 \cp ./static_ip.sh /var/ftp/share
 \cp /root/.ssh/id_rsa.pub /var/ftp/share/authorized_keys
 sleep 0.4
@@ -14,10 +15,8 @@ expect "login:" {send "root\n"}
 expect "密码：" {send "1\n"}
 expect "Password:" {send "1\n"}
 expect "#"      {send "yum -y install wget\n"}
-expect "#"      {send "wget ftp://192.168.1.254/share/static_ip.sh -O /root/static_ip.sh\n"}
-expect "#"      {send "bash static_ip.sh $3\n"}
-expect "#"      {send "systemctl restart network\n"}
-expect "#"      {send "echo\r"}
+expect "~]#"      {send "ls /root/static_ip.sh ||  wget ftp://192.168.1.254/share/static_ip.sh -O /root/static_ip.sh\n"}
+expect "~]#"    {send "bash static_ip.sh $3\n"}
 expect "#"      {send "exit\r"}
 EOF
 
@@ -30,5 +29,4 @@ expect "#"      {send "chmod 600 /root/.ssh/authorized_keys\n"}
 expect "#"      {send "hostnamectl set-hostname $2\n"}
 expect "#"      {send "yum repolist\n"}
 expect "#"      {send "exit\r"}
-set timeout 3
 EOF
