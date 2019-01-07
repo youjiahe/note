b999c858e702b01b24faa2c718e9674168a69c9f
youjiahe <youjiahe@163.com>
2019-01-07 09:58:33 +0800

    nginx-sticky

--- fibs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#斐波那挈数列
-a=(0 1)
-for i in `seq 0 20` 
-do
-   a[$[$i+2]]=$[${a[$i]}+${a[$[$i+1]]}]
-done
- echo ${a[*]}
--- httpd_check_stop_keepalived.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while :
-do
-   netstat -untlp | grep :80 &>/dev/null
-   [ $? -ne 0 ] && systemctl stop keepalived || systemctl start keepalived
-   sleep 5
-done
-
